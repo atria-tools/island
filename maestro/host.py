9973c3b4d6ac79aec4a0d42c604bfd4e96ba8e0e
Edouard DUPIN <yui.heero@gmail.com>
2017-03-19 22:07:09 +0100

    [DEV] better interface of basic binary

--- maestro/host.py
+++ maestro/host.py
@@ -15,23 +15,12 @@ from . import debug
 # print os.name # ==> 'posix'
 if platform.system() == "Linux":
 	OS = "Linux"
-	HOST_DEFAULT_COMPILATOR = "gcc"
 elif platform.system() == "Windows":
 	OS = "Windows"
-	HOST_DEFAULT_COMPILATOR = "gcc"
 elif platform.system() == "Darwin":
 	OS = "MacOs"
-	HOST_DEFAULT_COMPILATOR = "clang"
 else:
 	debug.error("Unknow the Host OS ... '" + platform.system() + "'")
 
 debug.debug("host.OS = " + OS)
-
-
-if sys.maxsize > 2**32:
-	BUS_SIZE = 64
-else:
-	BUS_SIZE = 32
-
-debug.debug("host.BUS_SIZE = " + str(BUS_SIZE))
 
